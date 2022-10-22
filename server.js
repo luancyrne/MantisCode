053f7f9a265683a7e491123ac5c48377dde58bb3
Luan Cyrne <luancv@outlook.com.br>
2022-10-22 01:10:03 -0300

    Update http module

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Aqui importamos o arquivo responsável por iniciar o bot do discord e outros modulos
 require('./src/bot/index');
 require('dotenv/config');
-const http = require('http').createServer()
+// const http = require('http').createServer()
 
 
 
@@ -9,6 +9,6 @@ const http = require('http').createServer()
 // Pra este projeto a utilização do modulo http não e necessário somente o arquivo do discord sera suficiente, 
 // porém para projetos futuros utilizaremos o modulo portanto ele esta aqui somente para acelerar a produção.
 //Fique a vontade para retirar toda a logica do modulo http contida neste arquivo
-http.listen(process.env.SERVER_PORT, () => {
-  console.log("Servidor MantisBot");
-})
+// http.listen(process.env.SERVER_PORT, () => {
+//   console.log("Servidor MantisBot");
+// })
